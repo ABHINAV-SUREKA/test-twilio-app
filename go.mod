7bddbd5bf462d576c22c26a17a182ae649bb7c0f
ABHINAV SUREKA <7731608+ABHINAV-SUREKA@users.noreply.github.com>
2024-02-15 01:44:39 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ABHINAV-SUREKA/test-twilio-app
+
+go 1.21.4
+
+require github.com/twilio/twilio-go v1.18.0
+
+require (
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+// Download the helper library from https://www.twilio.com/docs/go/install
+package main
+
+import (
+	"fmt"
+
+	"github.com/twilio/twilio-go"
+	api "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func main() {
+	// Find your Account SID and Auth Token at twilio.com/console
+	// and set the environment variables. See http://twil.io/secure
+	client := twilio.NewRestClient()
+
+	params := &api.CreateMessageParams{}
+	params.SetBody("This is the ship that made the Kessel Run in fourteen parsecs?")
+	params.SetFrom("+14847256474")
+	params.SetMediaUrl([]string{"https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg"})
+	params.SetTo("+919740446065")
+
+	resp, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		if resp.Sid != nil {
+			fmt.Println(*resp.Sid)
+		} else {
+			fmt.Println(resp.Sid)
+		}
+	}
+}
